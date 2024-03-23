7c04c82e3ca426ef4594368d7432d43c5dbff976
Marcus Benoit <benoit.marcus@gmail.com>
2024-03-23 17:15:58 +0100

    playing around with dynamic rows

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -10,9 +10,28 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         List {
-            ForEach(0..<10) {
-                Text("this is row number: \($0)")
+            Section("Section 1") {
+                Text("Static row 1")
+                Text("Static row 2")
             }
+            
+            Section("Section 2") {
+                ForEach(0..<4) {
+                    Text("Dynamic row: \($0 + 1)")
+                }
+            }
+            
+            
+            
+            Section("Section 3") {
+                Text("Static row 3")
+                Text("Static row 4")
+            }
+        }
+        .listStyle(.grouped)
+        
+        List(0..<4) {
+            Text("dynamic list \($0)")
         }
     }
 }
